a4e550c602b95fefc86c2ea2d7ee76cdd0d3d10a
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-29 13:15:17 -0300

    iniciando criacao do SearchExercises component

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,5 +1,24 @@
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+
 export function SearchExercises() {
   return (
-    <div>SearchExercises</div>
+    <Stack
+      alignItems="center"
+      justifyContent="center"
+      mt="37px"
+      p="20px"
+    >
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: '44px', xs: '30px' },
+          marginTop: { lg: '40px' }
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Exercícios Incríveis que <br /> Você Deve Conhecer
+      </Typography>
+    </Stack>
   )
 }
